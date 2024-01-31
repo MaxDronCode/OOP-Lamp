180e0eaed24be9858848ec89444b16685f343881
Max Dron <maxdron@icloud.com>
2024-01-31 17:15:00 +0100

    The main file is modified to include the commands that fulfill the purpose of this exercise.

--- src/main/kotlin/controllers/Main.kt
+++ src/main/kotlin/controllers/Main.kt
@@ -3,5 +3,32 @@ package controllers
 import models.Lamp
 
 fun main() {
+    println("------------------LAMP 1------------------")
+    val kitchenLamp : Lamp = Lamp("KitchenLamp")
+    kitchenLamp.turnOn()
+    kitchenLamp.changeColor()
+    kitchenLamp.changeColor()
+    kitchenLamp.changeColor()
+    kitchenLamp.intensityUp()
+    kitchenLamp.intensityUp()
+    kitchenLamp.intensityUp()
+    kitchenLamp.intensityUp()
 
+    println("------------------LAMP 2------------------")
+    val livingRoomLamp : Lamp = Lamp("LivingRoom")
+    livingRoomLamp.turnOn()
+    livingRoomLamp.changeColor()
+    livingRoomLamp.changeColor()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.turnOff()
+    livingRoomLamp.changeColor()
+    livingRoomLamp.turnOn()
+    livingRoomLamp.changeColor()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
+    livingRoomLamp.intensityUp()
 }
